6f092455c3bce857243dbaa425465f43569621e6
Ali Allam <146081536+aliallam123@users.noreply.github.com>
2025-12-28 10:28:10 +0000

    Create answer.py

---
+++ LeetCode Questions/0217 - Contains Duplicate/answer.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        x = set(nums)
+        if len(x) != len(nums):
+            return True
+        else:
+            return False
+        
